705aa8ab86cc03673ad013f19b5efdc8468d6de8
Jose Rodriguez <joser.20121999@gmail.com>
2026-09-03 09:35:44 +0000

    feat(e19): agregar bandeja web de garantias

--- server.js
+++ server.js
@@ -144,6 +144,7 @@ export function buildApp({ dbPath, sessionSecret, wooCfg, geminiKey, mlCfg, mobi
   app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
   app.use('/stock', express.static(path.join(__dirname, 'public/stock')));
   app.use('/excepciones', express.static(path.join(__dirname, 'public/excepciones')));
+  app.use('/garantias', express.static(path.join(__dirname, 'public/garantias')));
   app.use('/etiquetas', express.static(path.join(__dirname, 'public/etiquetas')));
   app.use('/inventario', express.static(path.join(__dirname, 'public/inventario')));
   app.use('/home', express.static(path.join(__dirname, 'public/home')));
